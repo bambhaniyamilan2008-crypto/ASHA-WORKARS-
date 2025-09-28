448b28466c43a10284db7e6ed3f910e2de5fdcc9
BAMBHANIYA MILAN <bambhaniyamilan.2008@gmail.com>
2025-09-28 16:09:22 +0530

    moved client files to root

--- client/src/components/examples/LanguageToggle.tsx
+++
@@ -1,9 +0,0 @@
-import LanguageToggle from '../LanguageToggle';
-
-export default function LanguageToggleExample() {
-  return (
-    <div className="p-6 flex justify-center">
-      <LanguageToggle />
-    </div>
-  );
-}
--- client/src/pages/home-page.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardLayout from "@/components/DashboardLayout";
-
-export default function HomePage() {
-  return <DashboardLayout />;
-}
